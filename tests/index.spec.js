d4001e6deb1062d5f16463a87d8077cd1fe4e30c
Paulo Augusto Nogueira Foroni <paulo.foroni@linx.com.br>
2019-09-18 17:45:31 -0300

    finalizado 108

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -11,5 +11,29 @@ global.fetch = require('node-fetch');
 import SpotifyWrapper from '../src/index';
 
 describe('SpotifyWrapper Library', () => {
+    it('should create an instance of SpotifyWrapper', () => {
+        let spotify = new SpotifyWrapper({});
+        expect(spotify).to.be.an.instanceof(SpotifyWrapper);
+    });
 
+    it('should receive apiURL as an option', () => {
+        let spotify = new SpotifyWrapper({
+            apiURL: 'blabla'
+        });
+
+        expect(spotify.apiURL).to.equal('blabla');
+    });
+
+    it('should use the default apiURL if not provided', () => {
+        let spotify = new SpotifyWrapper({});
+         expect(spotify.apiURL).to.be.equal('https://api.spotify.com/v1');
+    });
+
+    it('should receive token as an option', () => {
+        let spotify = new SpotifyWrapper({
+            token: 'foo'
+        });
+
+        expect(spotify.token).to.equal('foo');
+    });
 });
